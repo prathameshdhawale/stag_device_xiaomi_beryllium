b8e97390b67f8a596d3207dd7214affb06eb4c48
prathamesh <prathameshdhawale@gmail.com>
2019-07-25 01:35:15 +0000

    StagOs bringup for beryllium

---
+++ BoardConfig.mk
@@ -0,0 +1,44 @@
+#
+# Copyright (C) 2018-2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from sdm845-common
+-include device/xiaomi/sdm845-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/xiaomi/beryllium
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := beryllium
+
+# Audio
+AUDIO_FEATURE_ENABLED_FM_POWER_OPT := true
+
+# Camera
+TARGET_FACE_UNLOCK_CAMERA_ID := 5
+
+# Crypto
+TARGET_HW_DISK_ENCRYPTION := true
+
+# FM
+BOARD_HAS_QCA_FM_SOC := "cherokee"
+BOARD_HAVE_QCOM_FM := true
+
+# Kernel
+TARGET_KERNEL_CONFIG := beryllium_defconfig
+
+# HIDL
+DEVICE_FRAMEWORK_MANIFEST_FILE += $(DEVICE_PATH)/framework_manifest.xml
+
+# Partitions
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
+
+# Power
+TARGET_TAP_TO_WAKE_NODE := "/dev/input/event2"
+
+# Recovery
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
+
+# Inherit from the proprietary version
+-include vendor/xiaomi/beryllium/BoardConfigVendor.mk
